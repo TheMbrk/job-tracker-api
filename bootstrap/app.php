a9ed80734259160316ed586baf8c79279fb6d5d0
Mark Cyril Tubera <p.markcyriltubera@gmail.com>
2026-06-28 21:30:34 +0800

    Fix truncated bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,4 +17,6 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
-            fn (Request 
+            fn (Request $request) => $request->is('api/*'),
+        );
+    })->create();
